5001fc74a0e7167fbdae1643a9734d4be1b95295
popopododo <smartpodo@kakao.com>
2021-05-22 17:16:09 +0900

    Led Flask control

--- flask_ledcontrol/led_flask.py
+++ flask_ledcontrol/led_flask.py
@@ -8,28 +8,40 @@ LED = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED,GPIO.OUT)
 
-'''
-@app.route('/led/')
-def ledswitch():
-    
-    return render_template("index.html")
-
-
-
-
-'''
 @app.route('/') # Decorator
 def hello():
     return '<font size=10> HELLO, LED! </font>'
+
+@app.route('/led/')
+def led():
+    return render_template("index.html")
+
+
 @app.route('/led/on')
 def led_on():
     GPIO.output(LED,GPIO.HIGH)
-    return '<font size=10> LED{} ON! </font>'.format(LED)
+    return '<font size=10> HELLO, LED! </font>'
+
 
 @app.route('/led/off')
 def led_off():
     GPIO.output(LED,GPIO.LOW)
-    return '<font size=10> LED{} OFF! </font>'.format(LED)
+    return '<font size=10> GOOD BYE, LED! </font>'
 
+
+'''
+
+
+@app.route('/led/on')
+def led_on():
+    GPIO.output(LED,GPIO.HIGH)
+    return '<font size=10> LED on </font>'
+        
+
+@app.route('/led/off', methods=['POST')
+def led_off():
+    GPIO.output(LED,GPIO.LOW)
+    return '<font size=10> LED off </font>'
+ '''   
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=False)
