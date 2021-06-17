6486068e31301b42d4aeffcd565e0f9e01857015
popopododo <smartpodo@kakao.com>
2021-06-17 21:30:36 +0900

    Sending Temperature with Flask

--- flask_temp/flask_tem.py
+++ flask_temp/flask_tem.py
@@ -23,6 +23,7 @@ def get_temp():
 def index():
     return render_template('index.html')
 '''
-        
+'''        
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=False)
+'''
